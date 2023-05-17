3b13be0d21e376f563cd94ff093ffceaaa8e7c1c
asmraihan <asmraihanctg14@gmail.com>
2023-05-18 01:56:18 +0600

    testi

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 
 import Banner from '../../components/Banner/Banner';
+import Testimonial from '../../components/Testimonial/Testimonial';
 const Home = () => {
     return (
         
        <div>
          <Banner></Banner>
+         <Testimonial></Testimonial>
        </div>
 
     );
