ddf3101ac5df30a2aaed20f7365130981d7afca1
asmraihan <asmraihanctg14@gmail.com>
2023-05-18 00:36:34 +0600

    banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,12 @@
-import { FaArrowRight } from 'react-icons/fa';
+
+import Banner from '../../components/Banner/Banner';
 const Home = () => {
     return (
-        <h1>this home</h1>
-    //     <div className="svg-wrapper">
-    //     <svg height="40" width="150" xmlns="http://www.w3.org/2000/svg">
-    //       <rect id="shape" height="40" width="150" />
-    //     </svg>
-    //     <div className="text" id="see_all_btn">
-    //       <a to="/services" className="flex justify-center items-center">
-    //         See All
-    //         <FaArrowRight className="ml-2 mt-1" />
-    //       </a>
-    //     </div>
-    //   </div>
+        
+       <div>
+         <Banner></Banner>
+       </div>
+
     );
 };
 
